1ad1900674e33086097582b2c5c7793ebbb4b7b8
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 22:53:33 +0530

    ncurses1 - window printing

---
+++ ncurses.c
@@ -0,0 +1,26 @@
+#include <ncurses.h>
+
+int main()
+
+{
+
+    char letter;
+
+    // ncurses manages text-based windows on the terminal screen
+
+    initscr(); // initialize ncurses and creates default window
+
+    printw("Press any key : "); // prints text to the current ncurses window
+
+    refresh(); // update the terminal with changes made to the window
+
+    letter = getch();
+
+    clear(); // clear the current window and marks it for redraw
+
+    printw("you pushed: '%c'", letter);
+
+    refresh(); // update the terminal with the changes made to the window
+
+    getch(); // wait for a key press so that the output remains visible
+}
