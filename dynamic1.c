6e70fa4f9c0266dadb2ca8ed3ec4e0a88ecad8e5
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 22:56:31 +0530

    malloc()

--- dynamic1.c
+++ dynamic1.c
@@ -1,20 +1,17 @@
 /* dynamic1.c */
 
-#include<stdio.h>
-#include<stdlib.h> //contains the "malloc()" function 
+#include <stdio.h>
+#include <stdlib.h> //contains the "malloc()" function
 
 int main()
 
 {
- char *name;
- name = malloc(32); // dynamic allocation of 32 bytes of memory 
- printf("what is your name ?\n");
- scanf("%s",name); //here the name of the string "name" - decays into pointer to the first character - "&name[0]", therefore no need of "&"  
- printf("Hello %s\n",name);
- 
- free(name); // good habit to always free the memory allocation
- return 0;
+    char *name;
+    name = malloc(32); // dynamic allocation of 32 bytes of memory
+    printf("what is your name ?\n");
+    scanf("%s", name); // here the name of the string "name" - decays into pointer to the first character - "&name[0]", therefore no need of "&"
+    printf("Hello %s\n", name);
 
+    free(name); // good habit to always free the memory allocation
+    return 0;
 }
-
-
