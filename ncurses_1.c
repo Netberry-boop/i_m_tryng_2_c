a531bd4f6217cf49e128a41d11701a287ad083b9
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-02 19:42:39 +0530

    getyx() - coordinates

---
+++ ncurses_1.c
@@ -0,0 +1,29 @@
+
+/* ncurses_1.c */
+#include<ncurses.h>
+
+int main(){
+    int x , y;
+    initscr();
+    clear();
+
+    getyx(stdscr,y,x);// it locates the current positon of cursor on the screen and gets their coordinates
+    
+    printw("x = %d\n , y = %d\n ",x,y);
+    refresh();
+    
+    
+    y = 5 ; // 5 lines down  
+    x = 10 ; // 10 characters to the right
+
+    move(y,x);
+
+    printw("Over here!!");
+    refresh();
+    
+    
+    getch(); // holding the scree untill a key is pressed 
+    endwin();
+    
+    return 0;
+}
