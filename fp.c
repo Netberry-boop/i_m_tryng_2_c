156b3ed4fdfd6e73689afda96512030dde6fb6d6
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-04 22:39:55 +0530

    operations using function pointer - using learnt practices

--- fp.c
+++ fp.c
@@ -1,21 +1,25 @@
 /*fp.c*/
 // its a variable that points to another function (and use this variable to work with function)
 #include<stdio.h>
-
+#define F fflush(stdout)
+#define ADDITION '+'
+#define SUBTRACTION '-'
+#define MULTIPLICATION '*'
+#define DIVISION '/'
 void add(int *target, int a, int b){
 	*target = a+b; // if * not neighbour to then the * represents the action of dereferencing 
 	return ;
 }
 void subtract(int *target, int a, int b){
-        *target = a-b; // if * not neighbour to then the * represents the action of dereferencing 
+        *target = a-b;  
         return ;
 }
 void multiply(int *target, int a, int b){
-        *target = a*b; // if * not neighbour to then the * represents the action of dereferencing 
+        *target = a*b; 
         return ;
 }
 void divide(int *target, int a, int b){
-        *target = a/b; // if * not neighbour to then the * represents the action of dereferencing 
+        *target = a/b; 
         return ;
 }
 
@@ -23,34 +27,45 @@ int main(){
 	int a , b , c;
 	char op; 
 	printf("what operation do you want to perform ?(+,-,*,/) : ");
+	F;
 	scanf("%c",&op);
 	switch(op){
-	case '+' :
+	case ADDITION :
 		printf("enter numbers you want to add : ");
+		F;
 		scanf("%d%d",&a,&b);
 		add(&c,a,b);
 		printf("the sum is %d\n",c);
+		F;
 		break;
-	 case '-' :
+	 case SUBTRACTION :
                 printf("enter numbers you want to subtract : ");
-                scanf("%d%d",&a,&b);
+                F;
+		scanf("%d%d",&a,&b);
                 subtract(&c,a,b);
                 printf("the difference is %d\n",c);
-                break;
-	 case '*' :
+                F;
+		break;
+	 case MULTIPLICATION :
                 printf("enter numbers you want to multiply : ");
-                scanf("%d%d",&a,&b);
+                F;
+		scanf("%d%d",&a,&b);
                 multiply(&c,a,b);
                 printf("the product is %d\n",c);
-                break;
-	 case '/' :
+                F;
+		break;
+	 case DIVISION :
                 printf("enter numbers you want to divide : ");
-                scanf("%d%d",&a,&b);
+                F;
+		scanf("%d%d",&a,&b);
                 divide(&c,a,b);
                 printf("the quotient is %d\n",c);
-                break;
+                F;
+		break;
 	default : 
 		printf("Error , improper op_code\n");
+		F;
+		break;
 
 	}
 	return 0 ; 
